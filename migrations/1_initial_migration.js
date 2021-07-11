62a52fdbe96ea972a059ab71d459a745dc31299c
Rafael Medeiros <rafaelgurudesign@gmail.com>
2021-07-11 10:40:19 -0300

    config truffle

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const Exchange = artifacts.require("Exchange");
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(Exchange);
 };
